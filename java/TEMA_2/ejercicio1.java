0c074d7a82cab26201e78437201a3706dafb8d37
Sergiotaap <Sergiotapiarodri@gmail.com>
2022-11-02 17:25:10 +0100

    Se añaden cambios al archivo

--- java/TEMA_2/ejercicio1.java
+++ java/TEMA_2/ejercicio1.java
@@ -1,23 +1,21 @@
 package TEMA_2;
 
 public class ejercicio1 extends Thread{
+    private static int numHijos=4;
+    private static int numSaludos=1;
     public void run() {
+        for (int i=0;i<numSaludos;i++)
         System.out.println("En el Hilo "+getName()+" .. . ");
 	}
     
     public static void main(String[] args){
-		ejercicio1 hs1 = new ejercicio1();
-        hs1.setName("hijo1");
-        ejercicio1 hs2 = new ejercicio1();
-        hs2.setName("hijo2");
-        ejercicio1 hs3 = new ejercicio1();
-        hs3.setName("hijo3");
-        ejercicio1 hs4 = new ejercicio1();		
-        hs4.setName("hijo4"); 
-        hs1.run();
-        hs2.run();
-        hs3.run();
-        hs4.run();
+        for (int i=1;i<numHijos+1;i++){
+            ejercicio1 hs = new ejercicio1();
+            hs.setName("hijo "+i);
+            hs.start();
+        }
+        
+        
 		System.out.println("Fuera de los hilos ..");
 	}
 }
