f7d1fc81ffcb19120dcc1ee7219c106b39002637
Sergiotaap <Sergiotapiarodri@gmail.com>
2022-11-08 20:02:12 +0100

    Se añaden los últimos cambios al relojSwing

--- java/TEMA_2/relojSwing.java
+++ java/TEMA_2/relojSwing.java
@@ -1,48 +1,54 @@
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.*;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class relojSwing extends JFrame  {
-    public relojSwing(){
-        super("Reloj");
-        JLabel l= new JLabel();
-        l.setText("0");
-        
-        setLayout(new FlowLayout());
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 
-        getContentPane().add(l);
+public class relojSwing extends JFrame{
 
-        JButton b = new JButton("¡Comienza!");
-        getContentPane().add(b);
-
-           
-        Contador c= new Contador(l);
-        Thread t= new Thread(c);
-        b.addActionListener(new ActionListener(){
-           @Override
-           public void actionPerformed(ActionEvent e){
-            if(t.isAlive()){
-                t.resume();
-            }else{
-              
-              t.start();
-            }
-         }
-        });
-        JButton bPara = new JButton("¡Para!");
-        getContentPane().add(bPara);
-        bPara.addActionListener(new ActionListener(){
-            @Override
-                public void actionPerformed(ActionEvent e){
-                    t.suspend();
-                }
-        });
-
-        setSize(500, 400);
-        setVisible(true); //
-    }
-    public static void main(String[] args) {
-        new relojSwing();
-        
-      }
+	public relojSwing() {
+		super("Reloj");
+		
+		setLayout(new FlowLayout());
+		
+		JLabel l = new JLabel();
+		l.setText("0");
+		getContentPane().add(l);
+		
+		JButton b = new JButton("¡Comienza!");
+		getContentPane().add(b);
+		
+		Object sync = new Object();
+		
+		Contador c = new Contador(l, sync);
+		Thread t = new Thread(c);
+		t.start();
+		
+		b.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				synchronized(sync) {
+					sync.notify();
+				}
+			}
+		});
+		
+		JButton bPara = new JButton("¡Para!");
+		getContentPane().add(bPara);
+		bPara.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				t.interrupt();
+			}
+		});
+		
+		setSize(500,400);
+		setVisible(true);
+	}
+	
+	public static void main(String[] args) {
+		new relojSwing();
+	}
 }
