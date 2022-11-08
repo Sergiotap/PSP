350d8507bfcbee671e24ff24bd39d81e4e0048b3
Sergiotaap <Sergiotapiarodri@gmail.com>
2022-11-08 20:01:43 +0100

    Se añaden los últimos cambios al contador

--- java/TEMA_2/Contador.java
+++ java/TEMA_2/Contador.java
@@ -1,24 +1,42 @@
 import javax.swing.JLabel;
 
-public class Contador implements Runnable{
-    private static final long INTERVALO=1;
-    JLabel jSalida;
-    int cont =0;
-    boolean prueba=true;
-    public Contador(JLabel salida){
-        jSalida=salida;
-    }
-    public void run(){
-        try {
-            //No entra aquí
-            while(prueba ==true){
-                cont++;
-                jSalida.setText(Integer.toString(cont));
-                Thread.sleep(INTERVALO);
-            }
-        } catch (InterruptedException e) {
-            // TODO: handle exception
-            e.printStackTrace();
-        }
-    }
+public class Contador implements Runnable {
+	private static final long INTERVALO = 1;
+	JLabel _salida;
+	Object _sync;
+	
+	int cont = 0;
+	
+	public Contador(JLabel salida, Object sync) {
+		_salida = salida;
+		_sync = sync;
+	}
+	
+	private void parar() {
+		try {
+			synchronized(_sync) {
+				_sync.wait();
+			}
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			//e.printStackTrace();
+		}
+	}
+	
+	@Override
+	public void run() {
+		parar();
+		
+		while(true) {
+			cont++;
+			_salida.setText(Integer.toString(cont));
+			
+			try {
+				Thread.sleep(INTERVALO);
+			} catch (InterruptedException e) {
+				//e.printStackTrace();
+				parar();
+			}
+		}
+	}
 }
