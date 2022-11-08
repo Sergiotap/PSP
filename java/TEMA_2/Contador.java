7c4c8acec1d46418f6faf18cbb771ed8de370780
Sergiotaap <Sergiotapiarodri@gmail.com>
2022-11-08 19:32:35 +0100

    Se añaden cambios al contador

--- java/TEMA_2/Contador.java
+++ java/TEMA_2/Contador.java
@@ -1,15 +1,17 @@
 import javax.swing.JLabel;
 
 public class Contador implements Runnable{
-    private static final long INTERVALO=100;
+    private static final long INTERVALO=1;
     JLabel jSalida;
     int cont =0;
+    boolean prueba=true;
     public Contador(JLabel salida){
         jSalida=salida;
     }
     public void run(){
         try {
-            while(true){
+            //No entra aquí
+            while(prueba ==true){
                 cont++;
                 jSalida.setText(Integer.toString(cont));
                 Thread.sleep(INTERVALO);
