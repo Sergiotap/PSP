ab657b2de0b2672ac7e5651a3684dee5ced5a515
Sergiotaap <Sergiotapiarodri@gmail.com>
2022-11-15 19:26:30 +0100

    Se añade el archivo del hilo a

---
+++ java/TEMA_2/HiloA.java
@@ -0,0 +1,19 @@
+class HiloA extends Thread {
+	private Contador contador;
+	public HiloA (String n , Contador c) {
+		setName(n);
+		contador = c ;
+	}
+	
+	public void run(){
+		for (int j = 0; j < 200; j++) {
+			// 2º.- Posible sincro
+			synchronized(contador){
+				contador.incrementa();
+			}
+			try {
+				sleep(10);
+			} catch (InterruptedException e ) {}
+		}
+	}
+}
