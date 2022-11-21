ba36b6e4c290b7a0e75aafb28b47afc67ccaefc8
Sergiotaap <Sergiotapiarodri@gmail.com>
2022-11-21 17:59:24 +0100

    Se añaden los cambios a los .java y .class

--- repaso/usarC.java
+++ repaso/usarC.java
@@ -1,6 +1,9 @@
 package repaso;
 
 import java.io.InputStream;
+import java.io.OutputStream;
+
+import javax.print.attribute.standard.OutputDeviceAssigned;
 
 public class usarC {
    public static void main(String[] args) {
@@ -10,8 +13,15 @@ public class usarC {
         int c;
         System.out.println("Antes del bucle");
         System.out.println();
-        while((c=is.read())!=0)
+        OutputStream os = p.getOutputStream();
+        os.write('3');
+        os.write('3');
+        os.write('3');
+        os.flush();
+        while((c=is.read())!=-1){
             System.out.print((char)c);
+        }
+        
         is.close();
     } catch (Exception e) {
         e.printStackTrace();
