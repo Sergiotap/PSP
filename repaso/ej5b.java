feb20847ba7a04ccb9989c2b9e4f1595a585b6b8
Sergiotaap <Sergiotapiarodri@gmail.com>
2022-11-22 01:08:09 +0100

    Se añaden los últimos cambios al ejercicio 5

--- repaso/ej5b.java
+++ repaso/ej5b.java
@@ -3,11 +3,15 @@ package repaso;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Scanner;
 
 public class ej5b {
     public static void main(String[] args) throws IOException{
         File directorio = new File("/home/sergiotap/.config/Code/User/workspaceStorage/772d8a9671cd0c9fdf6f7e3dc6d3d57f/redhat.java/jdt_ws/PSP_7e39c477/bin");
-		ProcessBuilder pb = new ProcessBuilder("java", "repaso.ej5","Cadena");
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Escribe la cadena: ");
+		String cadena =sc.next();
+		ProcessBuilder pb = new ProcessBuilder("java", "repaso.ej5",cadena);
 		pb.directory(directorio);
 		
 		System.out.printf("Directorio de trabajo: %s%n",pb.directory());
